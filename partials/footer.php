b4df8714b471537a387c7afe9dbeabbbffafb486
Luke White <lukewhiteplays@gmail.com>
2013-12-26 18:44:09 -0500

    create partials

---
+++ partials/footer.php
@@ -0,0 +1,4 @@
+  	<script src="https://code.jquery.com/jquery.js"></script>
+  	<script src="/assets/js/vendor/bootstrap/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ partials/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>YooRecords</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
